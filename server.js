352d65bb62f08bc832da3011761ff2d8e3f0bd7a
Kevin Barrera Hernández <a01274402@HGOOSXCITLA04.local>
2016-12-13 16:09:09 -0600

    anadido

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+app.get('/', (req, res) => {
+	res.send('woooo!');
+});
+
+app.listen(8000, () => {
+	console.log('Server is running on port 8000');
+});
