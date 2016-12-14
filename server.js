1dfb45cc972bb8966ffafd1f40b3b8ce9827cbd8
Kevin Barrera Hernández <a01274402@HGOOSXCITLA04.local>
2016-12-13 18:18:27 -0600

    Trata de entender

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.get('/', (req, res) => {
-	res.send('Cambie de wooo a WAAAAAA!');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/otra_ruta', (req, res) => {
+	res.send('Esta es la nueva ruta');
+});
+
+app.post('/mi_ruta', (req, res) => {
+	console.log(req.body);
+	res.send('Comunicacion con el poljo');
 });
 
 app.listen(8000, () => {
